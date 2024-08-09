4e1c3037f89f3360fd3289584ea4d31338b384e8
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-09 20:38:03 -0300

    Atualizações
    Links, Imagens e outros

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,31 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <div className="containerGlobal">
+    <div className="containerGlobal">
         <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
         <h2>Exemplo h2</h2>
         <h3>Exemplo h3</h3>
         <p>Texto parágrafo</p>
+
+      <div className={styles.containerImage}>
+        <Image
+         src="/Growligth.png"
+         width={500}          //imagem com qualidade
+         height={500}
+         alt="Uma Imagem de um Pokemon"
+         className={styles.imagemHomePage}
+        />
+
+        <Image
+         src="/Growligth.png"
+         width={50}
+         height={50}       //Imagem sem qualidade
+         alt="Uma Imagem de um Pokemon"
+         className={styles.imagemHomePage}
+        />
       </div>
+
+    </div>
   );
 }
+
