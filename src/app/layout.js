394e2f7eac099f3d89d771c34472029cca0153ee
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-20 20:40:52 -0300

    Atualizações

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="pt-br">
-         <body>
+         <body suppressHydrationWarning={true}>
             <Cabecalho />
             {children}
             <Rodape />
