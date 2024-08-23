4fa856359303fac0d0d5678b8db4fb41eed12406
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-23 20:37:09 -0300

    att

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,30 @@
-import Image from "next/image";
+import {
+   MdLunchDining,
+   MdLocalBar,
+   MdDining,
+   MdIcecream,
+   MdFastfood,
+} from "react-icons/md";
+
 import styles from "./page.module.css";
+
+import Slider from "@/componentes/slider";
+import ProdutosHome from "@/componentes/produtos/produtosHome";
 
 export default function Home() {
    return (
       <div className="containerGlobal">
-         <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
-         <h2>Exemplo h2</h2>
-         <h3>Exemplo h3</h3>
-         <p>Texto parágrafo</p>
+         <Slider />
 
-         <div className={styles.containerImagem}>
-            <Image
-               src="/Growligth.png"
-               width={500}
-               height={500}
-               alt="Imagem"
-               className={styles.imagemHomePage}
-            />
-
-            <Image
-               src="/Growligth.png"
-               width={50}
-               height={50}
-               alt="Imagem"
-               className={styles.imagemHomePage}
-            />
+         <div className={styles.tipos}>
+            <MdLunchDining className={styles.tpicon} />
+            <MdLocalBar className={styles.tpicon} />
+            <MdDining className={styles.tpicon} />
+            <MdIcecream className={styles.tpicon} />
+            <MdFastfood className={styles.tpicon} />
          </div>
+
+         <ProdutosHome />
       </div>
    );
 }
